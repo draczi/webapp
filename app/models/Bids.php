53144ab4989d341b2a63b68c740411328e21ca2f
draczi <48152518+draczi@users.noreply.github.com>
2020-04-14 08:45:22 +0200

    Licit validatorok beallitasa, max licit

--- app/models/Bids.php
+++ app/models/Bids.php
@@ -19,8 +19,10 @@ class Bids extends Model {
         $this->runValidation(new UniqueValidator($this,['field'=>['user_id','product_id','deleted'],'msg'=>'Korábban már licitált, Ön vezeti a licitet. ']));
         $this->runValidation(new NumericValidator($this, ['field' => 'bid_amount', 'msg' => 'Licitként csak számot adhat meg!']));
         $this->runValidation(new RequiredValidator($this, ['field' => 'bid_amount', 'msg' => 'Kérlek adj meg egy licitet.']));
+        $this->runValidation(new RequiredValidator($this, ['field' => 'user_id', 'msg' => 'Csak bejelentkezett felhasználó licitálhat! Kérem jelentkezzen be.']));
         $this->runValidation(new DontMatchesValidator($this, ['field' => 'user_id', 'rule' => $this->vendor, 'msg' => 'A saját termékére nem licitálhat.']));
-        //$this->runValidation(new NumMinValidator($this,['field'=>'bid_amount','rule'=>$this->min_bid_price,'msg'=>'Licited a minimum alatt van! Kérlek adj meg egy összeget legalább ' . $this->min_bid_price . ' Ft értékben.']));
+        $this->runValidation(new NumMinValidator($this,['field'=>'bid_amount','rule'=>$this->min_bid_price,'msg'=>'Licited a minimum alatt van! Kérlek adj meg egy összeget legalább ' . $this->min_bid_price . ' Ft értékben.']));
+        $this->runValidation(new NumMaxValidator($this,['field'=>'bid_amount','rule'=>$this->min_bid_price*1.5,'msg'=>'Licited a megadható maximum összeg felett van! Kérlek adj meg egy összeget legfeljebb ' . $this->min_bid_price*1.5 . ' Ft értékig.']));
         $this->runValidation(new NumMinValidator($this,['field'=>'auction_end','rule'=>date('Y-m-d H:m:s'),'msg'=>'Az aukció lezárult! Sajnos a licited nem érvényes.']));
     }
 
